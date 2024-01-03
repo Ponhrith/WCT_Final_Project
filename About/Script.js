90bdc678564cd158b26b2aa4c38521a79777fb67
Ponhrith <ponhrith.nhep@gmail.com>
2024-01-03 22:20:07 +0700

    Testing

--- About/Script.js
+++ About/Script.js
@@ -26,7 +26,7 @@ if (user) {
 } else {
     // Revert to the original icon and link for not logged-in users
   iconElement.className = 'fa-solid fa-right-to-bracket';
-  linkElement.href = '../LogIn.html';
+  linkElement.href = '../auth/LogIn.html';
 }
 });
 
